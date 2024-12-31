cc8c0e7e50fa137ef79cd6a28b0a46cbb814f806
Taylor Schneider <taylorschneider07@yahoo.com>
2024-12-31 18:28:00 -0500

    Add biome sprites for later use

--- commands/world.js
+++ commands/world.js
@@ -1,4 +1,15 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+let XMLHttpRequest = require('xhr2');
+
+let biomes = {
+    "plain": ["Temperate", ""],
+    "arctic": ["Arctic", ""],
+    "hell": ["Hell", ""],
+    "desert": ["Desert", ""],
+    "brain": ["Brain", ""],
+    "space": ["Space", ""],
+    "deep": ["Deep", ""]
+}
 
 module.exports = {
     local: false,
@@ -7,6 +18,7 @@ module.exports = {
         .setDescription(`Grab the available info about a world`)
         .addStringOption((option) => option
             .setName('name')
+            .setRequired(true)
             .setDescription('World you wish to know about')),
 	async execute(interaction) {
         const guild = interaction.guild;
@@ -22,8 +34,6 @@ module.exports = {
                 let json = xhttp.responseText;
                 profileEmbed.setDescription(xhttp.responseText);
                 interaction.reply({ embeds: [profileEmbed] });
-            } else {
-                interaction.reply("Command Failed");
             }
         };
         xhttp.open("GET", "http://v2202410239072292297.goodsrv.de:5003/v1/worlds?api_token=", true);
