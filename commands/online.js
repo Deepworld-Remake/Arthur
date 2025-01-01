929e7290e331382fbbc8d6b1cd7744043d7b8988
Taylor Schneider <taylorschneider07@yahoo.com>
2024-12-31 21:05:48 -0500

    Minor changes and improvements

--- commands/online.js
+++ commands/online.js
@@ -8,16 +8,16 @@ function getPlayersForList(interaction, page, info, callback) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let json = xhttp.responseText;
-            console.log(json);
             try {
                 let raw = JSON.parse(json);
-                let best = info[0] || {};
-                let players = info[1] || 0; 
-                let worlds = info[2] || 0; 
+                let best = {};
+                let players = 0; 
+                let worlds = 0;
+                if (page == 1) best = raw[0];
                 let needsToEnd = false;
                 for (let i = 0; i < raw.length; i++) {
                     let curWorld = raw[i];
-                    if (curWorld.players > best.players || 0) best = curWorld;
+                    if (curWorld.players > best.players) best = curWorld;
                     players += curWorld.players;
                     if (curWorld.players >= 1) worlds++;
                     else {
@@ -25,22 +25,24 @@ function getPlayersForList(interaction, page, info, callback) {
                         break;
                     }
                 }
+                console.log(best);
+                let newInfo = [best, players, worlds];
                 if (needsToEnd) {
-                    callback(info);
+                    console.log(newInfo);
+                    callback(newInfo);
                 } else {
-                    let newInfo = [best, players, worlds];
                     getPlayersForList(interaction, page + 1, newInfo, callback);
                 }
             } catch(e) {
                 if (interaction.user.id == global.botOwner)
-                    interaction.reply("There was a problem checking the API\n-# Debug: " + e);
+                    interaction.reply('There was a problem checking the API\n-# Debug: ' + e);
                 else
-                    interaction.reply("There was a problem checking the API");
+                    interaction.reply('There was a problem checking the API');
                 return;
             }
         }
     };
-    xhttp.open("GET", "http://v2202410239072292297.goodsrv.de:5003/v1/worlds?api_token=&sort=popularity&page=" + page, true);
+    xhttp.open('GET', 'http://v2202410239072292297.goodsrv.de:5003/v1/worlds?api_token=&sort=popularity&page=' + page, true);
     xhttp.send();
 }
 
@@ -57,19 +59,21 @@ module.exports = {
                     profileEmbed.setDescription(info[1] > 1 ? `There are ${info[1]} Players online ${info[2] > 1 ? `across ${info[2]} Worlds` : 'in 1 World'}` : `There is 1 Player online at the moment`)
                         .addFields({
                             name: 'Current Top World',
-                            value: 'test-' + info[1].name
-                        });
+                            value: `${info[0].name} - ${info[0].players == 1 ? '1 Player' : info[0].players + 'Players'}` || 'Undefined'
+                        })
+                        .setColor(global.color);
                     interaction.reply({ embeds: [profileEmbed] });
                 } else {
-                    profileEmbed.setDescription(`There is no one online at the moment`);
+                    profileEmbed.setDescription(`There is no one online at the moment`)
+                        .setColor(global.color);
                     interaction.reply({ embeds: [profileEmbed] });
                 }
             });
         } catch(e) {
             if (interaction.user.id == global.botOwner)
-                interaction.reply("There was a problem checking the API\n-# Debug: " + e);
+                interaction.reply('There was a problem checking the API\n-# Debug: ' + e);
             else
-                interaction.reply("There was a problem checking the API");
+                interaction.reply('There was a problem checking the API');
         }
 	},
 };
--- commands/world.js
+++ commands/world.js
@@ -2,13 +2,48 @@ const { EmbedBuilder, SlashCommandBuilder, inlineCode } = require('discord.js');
 let XMLHttpRequest = require('xhr2');
 
 let biomes = {
-    "plain": ["Temperate", "https://media.discordapp.net/attachments/1041397042582388919/1323794948843372636/plain.png"],
-    "arctic": ["Arctic", "https://cdn.discordapp.com/attachments/1041397042582388919/1323794949153755177/arctic.png"],
-    "hell": ["Hell", "https://cdn.discordapp.com/attachments/1041397042582388919/1323794949979902022/hell.png"],
-    "desert": ["Desert", "https://cdn.discordapp.com/attachments/1041397042582388919/1323794949782634569/desert.png"],
-    "brain": ["Brain", "https://cdn.discordapp.com/attachments/1041397042582388919/1323794949405278308/brain.png"],
-    "space": ["Space", "https://cdn.discordapp.com/attachments/1041397042582388919/1323794948629467198/space.png"],
-    "deep": ["Deep", "https://cdn.discordapp.com/attachments/1041397042582388919/1323794949602283652/deep.png"]
+    'plain': ['Temperate', 'https://media.discordapp.net/attachments/1041397042582388919/1323794948843372636/plain.png'],
+    'arctic': ['Arctic', 'https://cdn.discordapp.com/attachments/1041397042582388919/1323794949153755177/arctic.png'],
+    'hell': ['Hell', 'https://cdn.discordapp.com/attachments/1041397042582388919/1323794949979902022/hell.png'],
+    'desert': ['Desert', 'https://cdn.discordapp.com/attachments/1041397042582388919/1323794949782634569/desert.png'],
+    'brain': ['Brain', 'https://cdn.discordapp.com/attachments/1041397042582388919/1323794949405278308/brain.png'],
+    'space': ['Space', 'https://cdn.discordapp.com/attachments/1041397042582388919/1323794948629467198/space.png'],
+    'deep': ['Deep', 'https://cdn.discordapp.com/attachments/1041397042582388919/1323794949602283652/deep.png']
+}
+
+const levDist = (s, t) => {
+    if (!s.length) return t.length;
+    if (!t.length) return s.length;
+    const arr = [];
+    for (let i = 0; i <= t.length; i++) {
+        arr[i] = [i];
+        for (let j = 1; j <= s.length; j++) {
+            arr[i][j] = i === 0 ? j : Math.min(
+                arr[i - 1][j] + 1,
+                arr[i][j - 1] + 1,
+                arr[i - 1][j - 1] + (s[j - 1] === t[i - 1] ? 0 : 1)
+            );
+        }
+    }
+    return arr[t.length][s.length];
+};  
+
+function searchWorlds(page, info, callback) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let json = xhttp.responseText;
+            let raw = JSON.parse(json);
+            let distances = [];
+            let needsToEnd = false;
+            for (let i = 0; i < raw.length; i++) {
+                let world = raw[i];
+                distances.push();
+            }
+        }
+    };
+    xhttp.open('GET', 'http://v2202410239072292297.goodsrv.de:5003/v1/worlds?api_token=&name=' + info, true);
+    xhttp.send();
 }
 
 module.exports = {
@@ -22,48 +57,41 @@ module.exports = {
             .setDescription('World you wish to know about')),
 	async execute(interaction) {
         const profileEmbed = new EmbedBuilder();
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                let json = xhttp.responseText;
-                try {
-                    let raw = JSON.parse(json)[0];
-                    let fields = [{
-                        name: "Biome",
-                        value: biomes[raw.biome][0]
-                    },{ 
-                        name: "PVP", 
-                        value: raw.pvp ? "Enabled" : "Disabled",
-                        inline: true
-                    },
-                    // { 
-                    //     name: "Private", 
-                    //     value: raw.private ? "Yes" : "No" 
-                    // },
-                    { 
-                        name: "Protection", 
-                        value: raw.protected ? "Enabled" : "Disabled" ,
-                        inline: true
-                    },{
-                        name: "Generated",
-                        value: new Date(raw.gen_date).toDateString()
-                    }];
-                    profileEmbed
-                        .setTitle(raw.name)
-                        .setThumbnail(biomes[raw.biome][1])
-                        .setDescription(`${Math.round(raw.explored * 1000) / 10}% Explored`)
-                        .addFields(...fields)
-                        .setColor(global.color);
-                    interaction.reply({ embeds: [profileEmbed] });
-                } catch(e) {
-                    if (interaction.user.id == global.botOwner)
-                        interaction.reply("World not found, or not specified\n-# Debug: " + e);
-                    else
-                        interaction.reply("World not found, or not specified");
-                }
+        searchWorlds(1, interaction.options.getString('name'), (info) => {
+            try {
+                let fields = [{
+                    name: 'Biome',
+                    value: biomes[raw.biome][0]
+                },{ 
+                    name: 'PVP', 
+                    value: raw.pvp ? 'Enabled' : 'Disabled',
+                    inline: true
+                },
+                // { 
+                //     name: 'Private', 
+                //     value: raw.private ? 'Yes' : 'No' 
+                // },
+                { 
+                    name: 'Protection', 
+                    value: raw.protected ? 'Enabled' : 'Disabled' ,
+                    inline: true
+                },{
+                    name: 'Generated',
+                    value: new Date(raw.gen_date).toDateString()
+                }];
+                profileEmbed
+                    .setTitle(raw.name)
+                    .setThumbnail(biomes[raw.biome][1])
+                    .setDescription(`${Math.round(raw.explored * 1000) / 10}% Explored`)
+                    .addFields(...fields)
+                    .setColor(global.color);
+                interaction.reply({ embeds: [profileEmbed] });
+            } catch(e) {
+                if (interaction.user.id == global.botOwner)
+                    interaction.reply('World not found, or not specified\n-# Debug: ' + e);
+                else
+                    interaction.reply('World not found, or not specified');
             }
-        };
-        xhttp.open("GET", "http://v2202410239072292297.goodsrv.de:5003/v1/worlds?api_token=&name=" + interaction.options.getString("name"), true);
-        xhttp.send();
+        });
 	},
 };
